4f634315209e7d9dbc437406d1e6ea0e0ad72479
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-12-01 12:28:03 +0900

    Update HanronApp.py

--- HanronApp.py
+++ HanronApp.py
@@ -20,12 +20,12 @@ if st.session_state["user"] is None:
     firebase = pyrebase.initialize_app(firebaseConfig)
     auth = firebase.auth()
     
-    st.title("ログイン / 新規登録")
     choice = st.sidebar.selectbox("メニューを選択", ["ログイン", "新規登録"])
-    email = st.text_input("メールアドレス")
-    password = st.text_input("パスワード", type="password")
 
     if choice == "ログイン":
+        st.title("ログイン")
+        email = st.text_input("メールアドレス")
+        password = st.text_input("パスワード", type="password")
         if st.button("ログイン"):
             try:
                 user = auth.sign_in_with_email_and_password(email, password)
@@ -35,6 +35,9 @@ if st.session_state["user"] is None:
                 st.error("ログインに失敗しました")
 
     else:
+        st.title("新規登録")
+        email = st.text_input("メールアドレス")
+        password = st.text_input("パスワード", type="password")
         if st.button("アカウント作成"):
             try:
                 auth.create_user_with_email_and_password(email, password)
