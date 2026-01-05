6213845037fa9ffbbc361c2a887354f2eaf582b6
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2026-01-05 09:11:50 +0900

    Update HanronApp.py

--- HanronApp.py
+++ HanronApp.py
@@ -344,7 +344,7 @@ with st.sidebar:
         st.markdown(
             """
             <div style="
-                padding: 11.5rem 1rem;
+                padding: 11rem 1rem;
                 color: #888;
                 font-size: 0.9rem;
                 text-align: center;
