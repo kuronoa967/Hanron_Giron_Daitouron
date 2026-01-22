fa36f8aa700b65c0f230f0ea0cb5bd97d85363b5
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2026-01-22 10:57:57 +0900

    Increase padding in markdown for better layout

--- HanronApp.py
+++ HanronApp.py
@@ -325,7 +325,7 @@ with st.sidebar:
         st.markdown(
             """
             <div style="
-                padding: 30% 1rem;
+                padding: 80% 1rem;
                 color: #888;
                 font-size: 0.9rem;
                 text-align: center;
@@ -349,7 +349,7 @@ with st.sidebar:
             st.markdown(
                 """
                 <div style="
-                    padding: 30% 1rem;
+                    padding: 80% 1rem;
                     color: #888;
                     font-size: 0.9rem;
                     text-align: center;
