9c87ff3f391b276d992998a028bc4147acf0bea5
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2026-01-23 19:57:50 +0900

    Update HanronApp.py

--- HanronApp.py
+++ HanronApp.py
@@ -273,6 +273,7 @@ def show_account_page():
                     st.session_state.page = "chat"
                     st.rerun()
                 else:
+                    print("Auth error raw:", data)
                     st.error(show_auth_error(data))
 
     # -------------------------
